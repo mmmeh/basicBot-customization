721df5a56a9b9753f30064ee49ac3b44b6b35cad
mmmeh <davidcrowe96@gmail.com>
2015-07-09 18:49:33 -0500

    Update extension.js

--- extension.js
+++ extension.js
@@ -56,7 +56,7 @@
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                 if (!bot.commands.executable(this.rank, chat)) return void (0);
                 else {
-                    API.sendChat("/me Our website: http//www.trilon.org");
+                    API.sendChat("/me Our website: http://www.trilon.org");
                 }
             }
         };
