be5f565b378215dae153fccf3bee8825b57a5a33
mmmeh <davidcrowe96@gmail.com>
2015-07-09 18:36:52 -0500

    Update extension.js

--- extension.js
+++ extension.js
@@ -47,6 +47,19 @@
                 }
             }
         };
+        
+        bot.commands.baconCommand = {
+            command: 'hi',  //The command to be called. With the standard command literal this would be: !bacon
+            rank: 'user', //Minimum user permission to use the command
+            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("hi");
+                }
+            }
+        };
 
         //Load the chat package again to account for any changes
         bot.loadChat();
