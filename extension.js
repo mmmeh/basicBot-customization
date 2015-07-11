606daacb28e079a079b88a7cd499ad6826d1aaf1
mmmeh <davidcrowe96@gmail.com>
2015-07-11 14:44:07 -0500

    Update extension.js

--- extension.js
+++ extension.js
@@ -49,14 +49,14 @@
         };
         
         bot.commands.baconCommand = {
-            command: 'site',  //The command to be called. With the standard command literal this would be: !bacon
+            command: 'adblock',  //The command to be called. With the standard command literal this would be: !bacon
             rank: 'user', //Minimum user permission to use the command
             type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
             functionality: function (chat, cmd) {
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                 if (!bot.commands.executable(this.rank, chat)) return void (0);
                 else {
-                    API.sendChat("/me Our website: http://www.trilon.org");
+                    API.sendChat("/me Get AdBlock here for Chrome http://tinyurl.com/TrilonAdBlock");
                 }
             }
         };
