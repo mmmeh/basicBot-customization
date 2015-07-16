2295cf4191b4005abfc2fdef1804034f1709b1a0
mmmeh <davidcrowe96@gmail.com>
2015-07-15 22:57:04 -0500

    Update extension.js

--- extension.js
+++ extension.js
@@ -69,7 +69,7 @@
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                 if (!bot.commands.executable(this.rank, chat)) return void (0);
                 else {
-                    API.sendChat("/me hi is slanted");
+                    API.sendChat("/me hi dis is slanted");
                 }
             }
         };
